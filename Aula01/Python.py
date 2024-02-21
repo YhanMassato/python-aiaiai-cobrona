034b242c65a621f1d041191d89af43f24b436b3a
YhanMassato <yhanashihara@gmail.com>
2024-02-20 22:40:53 -0300

    tentativa de programa que faz raiz bhaskara

--- Aula01/Python.py
+++ Aula01/Python.py
@@ -1,3 +1,28 @@
-valorA = float(input("insira o valor de A "))
-valorB = float(input("Insira o valor de B")) 
-valorC = float(input("Insira o valor de C")) 
+import math
+
+a = float(input("insira o valor de A"))
+
+if a == 0 :
+    print('A não pode ser igual a 0, pois sua equação não existirá solução')
+    exit()
+b = float(input("Insira o valor de B")) 
+c = float(input("Insira o valor de C")) 
+
+delta = (b**2) - (4 * a * c)
+
+if delta < 0:
+    print("Sua equação não possui solução no campo dos números reais")
+    exit()
+
+elif delta == 0 :
+    x = -(b) / 2*a
+    print('Sua equação possui um resultado nos campos dos reais e ele equivale a {}' .format(x))
+
+else : 
+    delta = math.sqrt(delta)
+    x1 = (-(b) + delta) / 2 * a
+    x2 = (-(b) - delta) / 2 * a
+    
+print('Sua equação do segundo grau retornou os valores {} e {}' .format(x1, x2))
+    
+
