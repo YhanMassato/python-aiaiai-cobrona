fb107c4b1524ddfe5506996538d791e1aaa6fd1c
YhanMassato <yhanashihara@gmail.com>
2024-04-09 22:26:14 -0300

    Desafio concluido, porém com uma meia falha

--- AVALIACAO/avaliacao.py
+++ AVALIACAO/avaliacao.py
@@ -14,7 +14,13 @@ candidato2 = candidatos[1]
 candidato3 = candidatos[2]
 
 print("Obrigado, agora me diga a quantidade de votos totais")
-votosTotais = int(input("Número total de votos "))
+
+votosTotais = input("Número total de votos ")
+
+while votosTotais.isnumeric() != True :   
+    votosTotais = input("Número total de votos ")
+
+votosTotais = float(votosTotais)
 
 print(f"Certo, a candidatura possuirá {votosTotais} votos ao todo.")
 
@@ -40,33 +46,33 @@ print("Aqui está o resultado da votação:")
 if votosCandidato1 == votosCandidato2 and votosCandidato1 == votosCandidato3 :
     print(f"Todos empataram com a mesma quantidade de votos... \nQuantidade total de votos: para cada {votosCandidato1}")
       
-if votosCandidato1 > votosCandidato2 and votosCandidato1 > votosCandidato3 :
+elif votosCandidato1 > votosCandidato2 and votosCandidato1 > votosCandidato3 :
     if votosCandidato2 > votosCandidato3 :
         print(f"Resultados: \n1º Candidato {candidato1} com {votosCandidato1} \n2º Candidato {candidato2} com {votosCandidato2} \n3º Candidato {candidato3} com {votosCandidato3}")
     else :
         print(f"Resultados: \n1º Candidato {candidato1} com {votosCandidato1} \n2º Candidato {candidato3} com {votosCandidato3} \n3º Candidato {candidato2} com {votosCandidato2}")
         
-if votosCandidato2 > votosCandidato1 and votosCandidato2 > votosCandidato3 :
+elif votosCandidato2 > votosCandidato1 and votosCandidato2 > votosCandidato3 :
     if votosCandidato1 > votosCandidato3 :
         print(f"Resultados: \n1º Candidato {candidato2} com {votosCandidato2} \n2º Candidato {candidato1} com {votosCandidato1} \n3º Candidato {candidato3} com {votosCandidato3}")
     else :
         print(f"Resultados: \n1º Candidato {candidato2} com {votosCandidato2} \n2º Candidato {candidato3} com {votosCandidato3} \n3º Candidato {candidato1} com {votosCandidato1}")
         
-if votosCandidato3 > votosCandidato2 and votosCandidato3 > votosCandidato1 :
+elif votosCandidato3 > votosCandidato2 and votosCandidato3 > votosCandidato1 :
     if votosCandidato1 > votosCandidato2 :
         print(f"Resultados: \n1º Candidato {candidato3} com {votosCandidato3} \n2º Candidato {candidato1} com {votosCandidato1} \n3º Candidato {candidato2} com {votosCandidato2}")
     else :
         print(f"Resultados:\n 1º Candidato {candidato3} com {votosCandidato3} \n2º Candidato {candidato2} com {votosCandidato2} \n3º Candidato {candidato1} com {votosCandidato1}")
 
-if votosCandidato1 == votosCandidato2 and votosCandidato1 != votosCandidato3 :
+elif votosCandidato1 == votosCandidato2 and votosCandidato1 != votosCandidato3 :
     print(f"O candidato {candidato1} e o candidato {candidato2} empataram")
     print(f"Empate: {votosCandidato1} votos \nCandidato {candidato3} ficou com {votosCandidato3} voto.")
 
-if votosCandidato1 == votosCandidato3 and votosCandidato1 != votosCandidato2 :
+elif votosCandidato1 == votosCandidato3 and votosCandidato1 != votosCandidato2 :
     print(f"O candidato {candidato1} e o candidato {candidato3} empataram")
     print(f"Empate: {votosCandidato1} votos \nCandidato {candidato2} ficou com {votosCandidato2} voto.")
 
-if votosCandidato3 == votosCandidato2 and votosCandidato3 != votosCandidato1 :
+elif votosCandidato3 == votosCandidato2 and votosCandidato3 != votosCandidato1 :
     print(f"O candidato {candidato3} e o candidato {candidato2} empataram")
     print(f"Empate: {votosCandidato3} votos \nCandidato {candidato1} ficou com {votosCandidato1} voto.")
 
